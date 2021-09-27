c79dd0256ff2ba87d27ccaa5f37bcf7859adb4f5
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2021-09-27 22:05:46 +0200

    Minor update

--- scripts/crypto_data.py
+++ scripts/crypto_data.py
@@ -1,49 +1,43 @@
 from cryptofeed import FeedHandler
 from cryptofeed.defines import TRADES
-from cryptofeed.exchanges import Coinbase, Bitstamp, Binance
+from cryptofeed.exchanges import Binance
 from datetime import datetime
+from time import time
 
 # Total records to store for now
-# total_records = int(1e6)
-total_records = int(10)
+total_records = int(1e6)
 
 # List to store trades info
 trades = []
 
+start_time = time()
+
+# Callback for trade(s) feed
 async def trade(data, *args, **kwargs):
-    time, amount, price, symbol, timestamp = int(data.timestamp * 1000000), data.amount, data.price, data.symbol, datetime.fromtimestamp(data.timestamp)
 
     temp = {}
-
-    temp["time"] = time
-    temp["amount"] = amount
-    temp["symbol"] = symbol
-    temp["price"] = price
-    temp["timestamp"] = timestamp
+    temp["time"], temp["amount"], temp["price"], temp["symbol"], temp["timestamp"] = int(data.timestamp * 1000000), data.amount, data.price, data.symbol, datetime.fromtimestamp(data.timestamp)
 
     trades.append(temp)
 
+    # Save & exit when we reach our desired no. of records
     if len(trades) == total_records:
         import pandas as pd
-        import sys
         import asyncio
 
         df = pd.DataFrame.from_dict(trades)
         df.to_csv(r'trades.csv', index=False, header=True)
 
+        print("Took {} seconds to collect {} records...".format( time() - start_time, total_records))
+
         loop = asyncio.get_event_loop()
         loop.stop()
 
-        # sys.exit(0)
-    # print("{}: {}\t{}\t{}\t{}\t{}\t".format(len(trades), time, amount, symbol, price, timestamp))
+        exit()
 
 def main():
-    # print("Time \t Amount \t Symbol \t Price \t Side \t Timestamp ")
     feed = FeedHandler()
-    # feed.add_feed(Coinbase(symbols=['BTC-USD'], channels=[TRADES], callbacks={TRADES: trade}))
-    # feed.add_feed(Bitstamp(symbols=['BTC-USD'], channels=[TRADES], callbacks={TRADES: trade}))
     feed.add_feed(Binance(symbols=['BTC-USDT'], channels=[TRADES], callbacks={TRADES: trade}))
-
     feed.run()
 
 if __name__ == "__main__":
