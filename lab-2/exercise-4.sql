aa539c3b44a9d71c2d8807decf001eddb22fc644
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2021-10-07 13:32:23 +0200

    Added exercise 4

---
+++ lab-2/exercise-4.sql
@@ -0,0 +1,18 @@
+use Lab_2
+
+/*
+-------------------------------
+Exercise - 4
+-------------------------------
+
+- The manager of a department must be an employee of that department.
+
+=> It can be done with triggers but we need two triggers here (one for each table "Employee" & "Department")
+
+*/
+
+select * from Employee
+
+alter table Employee
+
+
