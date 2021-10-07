e097911650f02c8182dc8162e8a1e6809e08d6c9
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2021-10-07 13:54:57 +0200

    Minor update

--- lab-2/exercise-4.sql
+++ lab-2/exercise-4.sql
@@ -12,7 +12,22 @@ Exercise - 4
 */
 
 select * from Employee
+select * from Department
+
+/*
+1.1 Add unique constraint for (SSN + Dept. No.) for Employee table
+*/
 
 alter table Employee
+add constraint manager_should_be_employee_of_that_department__employee_table
+unique ( SSN, DNo )
 
+/*
+1.2 Add foreign constraint for (MgrSSN + DNumber) for Department table with (SSN + Dept. No.) from Employee table
+*/
+
+alter table Department
+add constraint manager_should_be_employee_of_that_department__department_table
+foreign key( MgrSSN, DNumber )
+references Employee( SSN, DNo )
 
