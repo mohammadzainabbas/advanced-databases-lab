96259c3080f5e40146bf389853b2ca6dedf39ed8
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2021-10-14 16:03:17 +0200

    First query done

---
+++ lab-4/exercise-1.sql
@@ -0,0 +1,9 @@
+use temporal_db;
+
+select * from Employee
+select * from Department
+
+select E.FName as "First Name", E.LName as "Last Name"
+from Department D, Employee E, EmployeeAddress EA
+where D.MgrSSN = E.SSN and E.SSN = EA.SSN and EA.ToDate >= GETDATE() and EA.FromDate < GETDATE() and EA.City = 'Houston' and D.ToDate >= GETDATE()
+
