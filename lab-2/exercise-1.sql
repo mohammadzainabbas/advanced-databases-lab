45d84a576861c3aace52df31520fea71d941def9
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2022-01-24 11:12:07 +0100

    Minor update

--- lab-2/exercise-1.sql
+++ lab-2/exercise-1.sql
@@ -10,5 +10,10 @@ Exercise - 1
 */
 
 alter table Employee
-add CONSTRAINT age_no_greater_than_18
+add CONSTRAINT employee_age_greater_than_18
 CHECK( datediff( year, BDate, getdate() ) > 18 )
+
+-- delete from Employee where SSN = 1234321
+
+-- select * from Employee
+-- insert into Employee values ('Zain', 'M', 'Abbas', 1234321, DATEADD( YEAR, -19, GETDATE()), 'Avenue de la Couronne 365', 'M', 200000, null, 1, GETDATE());
